62b9670b5a0302cd76bb14d6ab44d8770d51d102
Yasuyuki Oka <yasuyk@gmail.com>
2016-11-03 10:52:03 +0900

    Bump version 0.3.0 -> 0.3.0-snapshot

--- helm-go-package.el
+++ helm-go-package.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2013-2016 Yasuyuki Oka
 
 ;; Author: Yasuyuki Oka <yasuyk@gmail.com>
-;; Version: 0.3.0
+;; Version: 0.3.0-snapshot
 ;; URL: https://github.com/yasuyk/helm-go-package
 ;; Package-Requires: ((emacs "24.4") (helm-core "2.2.1") (go-mode "1.4.0") (deferred "0.4.0"))
 
